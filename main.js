0217b3ac3a9170406a19d67aaca62db7d5ec31a3
Guilherme Rodrigues <guizaumnr@gmail.com>
2023-03-03 19:57:20 -0300

    Criando o icone do botao de menu (hamburger)

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(){
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault(); /* evitar o erro do evento do click ocorrer 2x */
+
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu); /* para diminuir o delay do click */
